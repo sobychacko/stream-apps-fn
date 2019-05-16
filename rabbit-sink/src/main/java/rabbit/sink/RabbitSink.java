086bb342517891510272a4ccbe85e067b3b399c2
Soby Chacko <schacko@pivotal.io>
2019-05-15 23:35:08 -0400

    rabbit sink -> boot app

--- rabbit-sink/src/main/java/rabbit/sink/RabbitSink.java
+++ rabbit-sink/src/main/java/rabbit/sink/RabbitSink.java
@@ -2,26 +2,34 @@ package rabbit.sink;
 
 import java.util.function.Function;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.support.MessageBuilder;
 
-public class RabbitSink implements Function<Object, Object> {
+@SpringBootApplication
+public class RabbitSink {
 
-	private final MessageHandler messageHandler;
+	@Autowired
+	private MessageHandler messageHandler;
 
-	public RabbitSink(MessageHandler messageHandler) {
-		this.messageHandler = messageHandler;
+	@Bean
+	public Function<Object, Object> sink() {
+		return o -> {
+			if(o instanceof Message) {
+				this.messageHandler.handleMessage((Message)o);
+			}
+			else {
+				this.messageHandler.handleMessage(MessageBuilder.withPayload(o).build());
+			}
+			return "Message sent to rabbitmq";
+		};
 	}
 
-	@Override
-	public Object apply(Object o) {
-		if(o instanceof Message) {
-			this.messageHandler.handleMessage((Message)o);
-		}
-		else {
-			this.messageHandler.handleMessage(MessageBuilder.withPayload(o).build());
-		}
-		return "Message sent to rabbitmq";
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitSink.class, args);
 	}
 }
